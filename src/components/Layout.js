7a41eb1533ad39c33b42a6760c01281a647b04a2
thenna1208 <thennavan.m@gmail.com>
2023-03-21 22:04:09 +0530

    Changed title from todo to Todo list application and heading color from blue to green

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ const Layout = () => {
     <div className='main-container'>
       <AuthStatus />
       <header className="app-title">
-        <h2>Todo Application </h2>
+        <h2>Todo List Application </h2>
       </header>
 
       <Outlet />
